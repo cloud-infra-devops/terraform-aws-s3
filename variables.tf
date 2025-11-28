72db1700d98548e39e09dd60bf323893dff6b8df
Pratik Das <pratik.x.das@gmail.com>
2025-11-28 08:43:55 -0600

    updated variables.tf file

--- variables.tf
+++ variables.tf
@@ -53,30 +53,30 @@ variable "versioning" {
     error_message = "S3 Bucket Versioning must be one of: Enabled or Disabled"
   }
 }
-variable "read_principals" {
-  description = "Principal allowed to read from the bucket (default: current account)"
-  type        = list(string)
-  default     = []
-}
-variable "read_tags" {
-  description = "Tags required on principals reading to the bucket"
-  type        = map(string)
-  default     = {}
-}
-variable "readwrite_principals" {
-  description = "Principal allowed to read and write to the bucket (default: current account)"
-  type        = list(string)
-  default     = []
-}
-variable "readwrite_tags" {
-  description = "Tags required on principals writing to the bucket"
-  type        = map(string)
-  default     = {}
-}
+# variable "read_principals" {
+#   description = "Principal allowed to read from the bucket (default: current account)"
+#   type        = list(string)
+#   default     = []
+# }
+# variable "read_tags" {
+#   description = "Tags required on principals reading to the bucket"
+#   type        = map(string)
+#   default     = {}
+# }
+# variable "readwrite_principals" {
+#   description = "Principal allowed to read and write to the bucket (default: current account)"
+#   type        = list(string)
+#   default     = []
+# }
+# variable "readwrite_tags" {
+#   description = "Tags required on principals writing to the bucket"
+#   type        = map(string)
+#   default     = {}
+# }
 variable "enable_kms" {
   description = "Enable KMS-backed server-side encryption (aws:kms). If false, AES256 will be used."
   type        = bool
-  default     = false
+  # default     = false
 }
 variable "kms_key_id" {
   description = "Optional KMS CMK ID or ARN to use for aws:kms encryption. If null, a new CMK will be created when `enable_kms` is true."
