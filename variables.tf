01ca75779e78fbba41504367e01a61952e00a200
Pratik Das <pratikdas@Pratiks-MacBook-Pro.local>
2025-12-01 21:41:27 -0600

    replaced null value for s3_name_prefix

--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
-# variable "bucket_policy" {
-#   description = "Override the resource policy on the bucket"
-#   type        = string
-#   default     = null
-# }
 variable "aws_region" {
   description = "AWS Region"
   type = string
   default = "us-west-2"
 }
+
+variable "aws_account_id" {
+  description = "The AWS account ID to deploy resources"
+  type        = string
+  default     = "211125325120" #Put AWS Account ID
+}
+
 variable "s3_name_prefix" {
   description = "The name of the s3 bucket"
   type        = string
-  default     = null
+  default     = "374278"
 }
 
 variable "tags" {
@@ -57,6 +59,28 @@ variable "versioning" {
     error_message = "S3 Bucket Versioning must be one of: Enabled or Disabled"
   }
 }
+
+variable "enable_kms" {
+  description = "Enable KMS-backed server-side encryption (aws:kms). If false, AES256 will be used."
+  type        = bool
+  default     = false
+  validation {
+    condition     = var.enable_kms != null ? var.enable_kms == true || var.enable_kms == false : true
+    error_message = "enable_kms must be a boolean value (true or false) or null"
+  }
+}
+variable "kms_key_id" {
+  description = "Optional KMS CMK ID or ARN to use for aws:kms encryption. If null, a new CMK will be created when `enable_kms` is true."
+  type        = string
+  default     = null
+}
+
+# variable "bucket_policy" {
+#   description = "Override the resource policy on the bucket"
+#   type        = string
+#   default     = null
+# }
+
 # variable "read_principals" {
 #   description = "Principal allowed to read from the bucket (default: current account)"
 #   type        = list(string)
@@ -77,17 +101,3 @@ variable "versioning" {
 #   type        = map(string)
 #   default     = {}
 # }
-variable "enable_kms" {
-  description = "Enable KMS-backed server-side encryption (aws:kms). If false, AES256 will be used."
-  type        = bool
-  default     = false
-  validation {
-    condition     = var.enable_kms != null ? var.enable_kms == true || var.enable_kms == false : true
-    error_message = "enable_kms must be a boolean value (true or false) or null"
-  }
-}
-variable "kms_key_id" {
-  description = "Optional KMS CMK ID or ARN to use for aws:kms encryption. If null, a new CMK will be created when `enable_kms` is true."
-  type        = string
-  default     = null
-}
